1975ec341097d2087f96a4a3894ccc3191f77372
teshan224 <teshan224@gmail.com>
2025-09-11 09:23:42 +0530

    Update createInstance.tf

--- createInstance.tf
+++ createInstance.tf
@@ -1,13 +1,14 @@
 provider "aws" {
   access_key = "AKIAVRUVTXIERJFY4FIP"
   secret_key = "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"
-  region = "us-east-2"
+  region = "eu-north-2"
 }
 
 resource "aws_instance" "MyFirstInstance" {
-    ami = "ami-0d9a665f802ae6227"
+    ami = "ami-000e50175c5f86214"
     instance_type = "t2.micro"
   
 
 }
+
 
